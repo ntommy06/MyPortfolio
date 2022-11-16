04b91a52f4393563a2739403299dc66efced3ffb
ntommy06 <tommyducnguyen@gmail.com>
2022-11-16 10:32:02 -0800

    Adding dependencies and initializing Navbar Component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello World</h1>);
+root.render(<App></App>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
